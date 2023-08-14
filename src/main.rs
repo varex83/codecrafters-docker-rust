58e5bd7b2cdff07b102fbe89b8eafd07a1b4a23b
varex83 <limposfeed@gmail.com>
2023-08-15 00:12:39 +0300

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+use std::process::{exit, Stdio};
 use anyhow::{Context, Result};
+use tokio::process::Command;
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage!
+#[tokio::main]
+async fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    let mut child = Command::new(command)
         .args(command_args)
-        .output()
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
@@ -19,12 +20,7 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
-    }
+    let exit_status = child.wait().await?;
 
-    Ok(())
+    exit(exit_status.code().unwrap_or(1));
 }
